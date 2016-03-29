44644f24c34450e344fa7292c5e2e44230f02310
luckcoding <luckcoding@gmail.com>
2016-03-29 22:26:43 +0800

    fix cros

--- app/middleware/cros.js
+++ app/middleware/cros.js
@@ -1,6 +1,7 @@
 export default async function (ctx, next) {
   ctx.set('Access-Control-Allow-Origin', '*')
-  if (ctx.request.header.origin != 'http://localhost:8080') {
+  ctx.set('Access-Control-Allow-Headers','content-type')
+  if (!ctx.request.header.origin.includes('http://localhost:8080')) {
     return false
   }
   await next()
